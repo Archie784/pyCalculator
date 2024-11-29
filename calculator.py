fc4977774221341470ce2b46937431284df8f585
ehidk <103606596+Archie784@users.noreply.github.com>
2024-11-29 23:11:26 +1100

    Update calculator.py
    added sqrt and imported 'math'

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math  # needed for the square root thing
+
 def add(x, y):
     return x + y
 
@@ -13,32 +15,39 @@ def divide(x, y):
     else:
         return x / y
 
+def square_root(x):
+    if x < 0:
+        return "Error: Cannot calculate the square root of a negative number."
+    return math.sqrt(x)
+# ask what type of calculation the user wants to do
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Square Root")
 
 while True:
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+    if choice in ('1', '2', '3', '4', '5'):
+        if choice == '5':
+            num = float(input("Enter number: "))
+            print("√", num, "=", square_root(num))
+        else:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        # Ask if the user wants to continue
+        # ask if user wants to do another calculation
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation.lower() != "yes":
             break
